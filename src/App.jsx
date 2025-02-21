882ad140c02cc0bccbdcf680fc77af8d44d9417d
sandhya1510k <sandhyabujji1510@gmail.com>
2025-02-21 22:50:38 +0530

    Added new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from '@/Components/Navbar/Navbar';
+import Home from '@/Components/Home/Home';
 
 function App() {
 
   return (
     <>
     <Navbar />
+    <Home />
+
     </>
   )
 }
